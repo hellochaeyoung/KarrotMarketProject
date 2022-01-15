e25b0b2688a3738e18fbe6662024dfbbffdb9ab5
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-01-15 15:51:48 +0900

    댓글 서비스 구현

---
+++ src/main/java/com/project/karrot/service/CommentService.java
@@ -0,0 +1,33 @@
+package com.project.karrot.service;
+
+import com.project.karrot.domain.Comment;
+import com.project.karrot.domain.Product;
+import com.project.karrot.repository.CommentRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    public Optional<Comment> find(Long commentId) {
+        return commentRepository.findById(commentId);
+    }
+
+    public List<Comment> findByProduct(Product product) {
+        return commentRepository.findByProduct(product);
+    }
+
+    public Comment register(Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    public void remove(Comment comment) {
+        commentRepository.delete(comment);
+    }
+}
