cf142bc7ac9bcb24b882b03e53f4a2104c679053
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-01-15 15:51:10 +0900

    거래 서비스 구현

---
+++ src/main/java/com/project/karrot/service/DealService.java
@@ -0,0 +1,33 @@
+package com.project.karrot.service;
+
+import com.project.karrot.domain.Deal;
+import com.project.karrot.domain.Member;
+import com.project.karrot.repository.DealRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class DealService {
+
+    private final DealRepository dealRepository;
+
+    public DealService(DealRepository dealRepository) {
+        this.dealRepository = dealRepository;
+    }
+
+    public Optional<Deal> find(Long dealId) {
+        return dealRepository.findById(dealId);
+    }
+
+    public List<Deal> findByMember(Member member) {
+        return dealRepository.findByMember(member);
+    }
+
+    public Deal register(Deal deal) {
+        return dealRepository.save(deal);
+    }
+
+    public void remove(Deal deal) {
+        dealRepository.delete(deal);
+    }
+}
