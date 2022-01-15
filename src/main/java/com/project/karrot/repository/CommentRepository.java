c04109ce0d42fae06245295ad59490dd92c37318
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-01-15 15:52:05 +0900

    댓글 레포지토리 구현

---
+++ src/main/java/com/project/karrot/repository/CommentRepository.java
@@ -0,0 +1,16 @@
+package com.project.karrot.repository;
+
+import com.project.karrot.domain.Comment;
+import com.project.karrot.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query( "select c from Comment c where c.product :product")
+    List<Comment> findByProduct(@Param("product") Product product);
+
+}
