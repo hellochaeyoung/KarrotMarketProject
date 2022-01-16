e6b0a26729455545e56fb6b4cc7434cc9c0f40cb
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-01-16 23:37:02 +0900

    지역 도메인 수정

--- src/main/java/com/project/karrot/domain/Location.java
+++ src/main/java/com/project/karrot/domain/Location.java
@@ -10,10 +10,7 @@ public class Location {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long locationId;
 
-    private String state;
-    private String city;
-    private String gu;
-    private String dong;
+    private String address;
 
     @OneToMany(mappedBy = "location")
     List<Product> products = new ArrayList<>();
@@ -45,36 +42,12 @@ public class Location {
         this.locationId = locationId;
     }
 
-    public String getState() {
-        return state;
+    public String getAddress() {
+        return address;
     }
 
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getGu() {
-        return gu;
-    }
-
-    public void setGu(String gu) {
-        this.gu = gu;
-    }
-
-    public String getDong() {
-        return dong;
-    }
-
-    public void setDong(String dong) {
-        this.dong = dong;
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
 
