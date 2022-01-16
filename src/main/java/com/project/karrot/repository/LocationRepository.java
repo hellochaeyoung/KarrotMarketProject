baa87636d96505a8b727b52259031fdaf7b24d4a
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-01-16 23:36:01 +0900

    지역 레포지토리 구현

---
+++ src/main/java/com/project/karrot/repository/LocationRepository.java
@@ -0,0 +1,15 @@
+package com.project.karrot.repository;
+
+import com.project.karrot.domain.Location;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface LocationRepository extends JpaRepository<Location, Long> {
+
+    @Query( "select l from Location l where l.address like %:name%")
+    List<Location> findByName(@Param("name") String name); // 그래야 메인화면에서 회원 지역의 상품들 지역 아이디로 갖고오기 가능
+
+}
