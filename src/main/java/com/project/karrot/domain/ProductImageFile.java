48d49bf4f855ccd83da3be103d2bb873b64e390f
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-01-16 23:39:23 +0900

    이미지파일 상속으로 인한 자식 클래스로 수정

--- src/main/java/com/project/karrot/domain/ProductImageFile.java
+++ src/main/java/com/project/karrot/domain/ProductImageFile.java
@@ -3,50 +3,12 @@ package com.project.karrot.domain;
 import javax.persistence.*;
 
 @Entity
-public class ProductImageFile {
-
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long fileId;
-
-    private String fileName;
-    private String fileOriName;
-    private String fileURL;
+@DiscriminatorValue("productImageFile")
+public class ProductImageFile extends ImageFiles {
 
     @ManyToOne
     @JoinColumn(name = "PRODUCT_ID")
     private Product product;
-
-    public long getFileId() {
-        return fileId;
-    }
-
-    public void setFileId(long fileId) {
-        this.fileId = fileId;
-    }
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
-
-    public String getFileOriName() {
-        return fileOriName;
-    }
-
-    public void setFileOriName(String fileOriName) {
-        this.fileOriName = fileOriName;
-    }
-
-    public String getFileURL() {
-        return fileURL;
-    }
-
-    public void setFileURL(String fileURL) {
-        this.fileURL = fileURL;
-    }
 
     public Product getProduct() {
         return product;
