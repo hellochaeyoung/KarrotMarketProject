10c7241d2b3cc69b460a074a305225a4efdbbd2f
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-01-18 00:36:28 +0900

    setProduct() 로직 수정

--- src/main/java/com/project/karrot/domain/Deal.java
+++ src/main/java/com/project/karrot/domain/Deal.java
@@ -36,13 +36,13 @@ public class Deal {
     }
 
     public void setProduct(Product product) {
-        this.product = product;
         if(product == null) { // 상품 자체 삭제 또는 상품 진행 단계 변경(거래완료 -> 예약중 or 거래완료 -> 판매중)으로 인한 연관관계 삭제
             this.product.setDeal(null); // 상품 - 거래 연관관계 삭제
             this.member.getDeals().remove(this); // 회원 - 거래 연관관계 삭제
         }else {
-            this.product.setDeal(this);
+            product.setDeal(this);
             this.member.getDeals().add(this); // 만약 거래완료에서 예약중으로 바꾸면 deal 목록에서 빼야하는디 이건 더 생각해서 해결하기
         }
+        this.product = product;
     }
 }
