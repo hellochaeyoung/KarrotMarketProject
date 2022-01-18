8538ed33fb6abbaded2d34866a8df9b9db6e87e4
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-01-18 23:38:16 +0900

    댓글 등록 DTO 구현

---
+++ src/main/java/com/project/karrot/controller/CommentForm.java
@@ -0,0 +1,14 @@
+package com.project.karrot.controller;
+
+public class CommentForm {
+
+    private String contents;
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+}
