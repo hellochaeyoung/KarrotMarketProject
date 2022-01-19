b5995ec1e6b0d3fb9a98d86a048efda2e3ff7e36
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-01-19 23:54:17 +0900

    MemberService 추가

--- src/main/java/com/project/karrot/controller/ProductController.java
+++ src/main/java/com/project/karrot/controller/ProductController.java
@@ -1,28 +1,26 @@
 package com.project.karrot.controller;
 
 import com.project.karrot.constants.SessionConstants;
-import com.project.karrot.domain.Comment;
-import com.project.karrot.domain.Member;
-import com.project.karrot.domain.Product;
+import com.project.karrot.domain.*;
 import com.project.karrot.service.CommentService;
+import com.project.karrot.service.MemberService;
 import com.project.karrot.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class ProductController {
 
+    private final MemberService memberService;
     private final ProductService productService;
     private final CommentService commentService;
 
-    public ProductController(ProductService productService, CommentService commentService) {
+    public ProductController(MemberService memberService, ProductService productService, CommentService commentService) {
+        this.memberService = memberService;
         this.productService = productService;
         this.commentService = commentService;
     }
@@ -38,7 +36,7 @@ public class ProductController {
         productList.remove(product);
         model.addAttribute("otherProducts", productList);
 
-        model.addAttribute("nickName", productOwner.getNickName());
+        model.addAttribute("productOwner", productOwner);
         //model.addAttribute("address", productOwner.getLocation().getAddress());
 
         return "/products/view";
@@ -59,19 +57,72 @@ public class ProductController {
 
     @PostMapping("/products/comment")
     public String registerComment(@SessionAttribute(name = SessionConstants.LOGIN_MEMBER, required = false) Member loginMember, CommentForm commentForm,
-                                  Long productId) {
+                                  Long productId, Model model) {
 
         Product product = productService.find(productId).orElseGet(Product::new);
 
-        System.out.println("############## " + productId);
         Comment comment = new Comment();
+
+        Optional<Long> existId = commentService.exist(loginMember, product);
+        existId.ifPresent(comment::setCommentId);
+
         comment.setContents(commentForm.getContents());
         comment.setMember(loginMember);
         comment.setProduct(product);
 
         commentService.register(comment);
 
-        return "/products/comment";
+        return "redirect:/products/comment?productId=" + productId;
 
+    }
+
+    @GetMapping("/products/all")
+    public String all(Model model, String nickName, Product product) {
+
+        Member member = memberService.findByNickName(nickName).orElseGet(Member::new);
+
+        //List<Product> products = productService.findByMember(member).orElseGet(ArrayList::new);
+
+        System.out.println("$$$$$$$$$$$$$$" + nickName);
+        List<Product> products = productService.findByMember(member).orElseGet(ArrayList::new);
+        model.addAttribute("products", products);
+
+        model.addAttribute("member", member);
+        model.addAttribute("product", product);
+
+        return "/products/all";
+    }
+
+    @PostMapping("/products/all")
+    public String allByStatus(Model model, StatusProductForm statusProductForm) {
+
+        List<Product> list = new ArrayList<>();
+
+        //System.out.println(map.get("member"));
+        //String status = map.get("status").toString();
+        //Member member = map.get("member");
+        System.out.println("%%%%%%%%%%%" + statusProductForm.getStatus());
+        System.out.println("%%%%%%%%%%%" + statusProductForm.getMemberId());
+
+        String status = statusProductForm.getStatus();
+        Long memberId = Long.parseLong(statusProductForm.getMemberId());
+
+        Member member = memberService.find(memberId).orElseGet(Member::new);
+
+        System.out.println("%%%%%%%%%" + status);
+        System.out.println("%%%%%%%%%" + member.getId());
+        if(status.equals("ALL")) {
+            list = productService.findByMember(member).orElseGet(ArrayList::new);
+        }else if(status.equals("SALE")) {
+            list = productService.findByMemberAndStatus(member, ProductStatus.SALE).orElseGet(ArrayList::new);
+        }else {
+            for(Deal deal : member.getDeals()) {
+                list.add(deal.getProduct());
+            }
+        }
+
+        model.addAttribute("products", list);
+
+        return "/products/all :: #resultTable";
     }
 }
