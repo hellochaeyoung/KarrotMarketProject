b9e66c473b45964f60871d1b519ecc5d8629e029
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-01-21 05:22:45 +0900

    이메일 중복 확인 로직 삭제

--- src/main/java/com/project/karrot/service/MemberService.java
+++ src/main/java/com/project/karrot/service/MemberService.java
@@ -38,12 +38,6 @@ public class MemberService {
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 닉네임입니다.");
                 });
-
-
-        memberRepository.findByEmail(member.getEmail())
-                .ifPresent(m -> {
-                    throw new IllegalStateException("이미 존재하는 이메일입니다.");
-                });
     }
 
     public Optional<Member> find(Long memberId) {
