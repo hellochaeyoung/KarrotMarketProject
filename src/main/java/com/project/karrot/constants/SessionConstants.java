3ca58aa57741ce8f28bbbede244e3e6e34a929b0
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-01-20 23:38:26 +0900

    로그인 계정 지역 정보 추가 저장

--- src/main/java/com/project/karrot/constants/SessionConstants.java
+++ src/main/java/com/project/karrot/constants/SessionConstants.java
@@ -3,5 +3,6 @@ package com.project.karrot.constants;
 public interface SessionConstants { // 이렇게 인터페이스로 하면 객체 생성을 막을 수 있다.
 
     String LOGIN_MEMBER = "loginMember";
+    String LOGIN_MEMBER_LOCATION = "loginLocation";
 
 }
