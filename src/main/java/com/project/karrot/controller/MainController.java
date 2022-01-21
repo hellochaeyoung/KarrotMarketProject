03d4cbb4058fdf8179b061f14a4db63953dc9e53
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-01-21 15:17:30 +0900

    상품 등록 시 진행상태, 지역, 게시시간 등록 로직 추가

--- src/main/java/com/project/karrot/controller/MainController.java
+++ src/main/java/com/project/karrot/controller/MainController.java
@@ -1,10 +1,7 @@
 package com.project.karrot.controller;
 
 import com.project.karrot.constants.SessionConstants;
-import com.project.karrot.domain.Category;
-import com.project.karrot.domain.Location;
-import com.project.karrot.domain.Member;
-import com.project.karrot.domain.Product;
+import com.project.karrot.domain.*;
 import com.project.karrot.service.CategoryService;
 import com.project.karrot.service.LocationService;
 import com.project.karrot.service.ProductService;
@@ -13,7 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -69,16 +68,26 @@ public class MainController {
         System.out.println("&&&&&&&&" + productForm.getCategory());
         Category category = categoryService.findByName(productForm.getCategory()).get();
 
+
         product.setProductName(productForm.getProductName());
         product.setPrice(productForm.getPrice());
         product.setCategory(category);
         product.setContents(productForm.getContents());
         product.setMember(loginMember); //////////////////////
+        product.setProductStatus(ProductStatus.SALE);
+        product.setLocation(loginMember.getLocation());
+        product.setTime(fomatDate());
         System.out.println("############" + loginMember.getNickName());
 
         productService.register(product); //////////////////
 
         return "redirect:/";
+    }
+
+    public String fomatDate() {
+        Date now = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd, HH:mm");
+        return format.format(now);
     }
 
     /*
