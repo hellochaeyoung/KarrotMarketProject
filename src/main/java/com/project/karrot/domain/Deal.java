ce9f9a107f421c565b110a79cc01191c5273f617
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-01-21 15:16:51 +0900

    거래 - 상품 연관관계 수정

--- src/main/java/com/project/karrot/domain/Deal.java
+++ src/main/java/com/project/karrot/domain/Deal.java
@@ -12,7 +12,8 @@ public class Deal {
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
-    @OneToOne(mappedBy = "deal")
+    @OneToOne
+    @JoinColumn(name = "PRODUCT_ID")
     private Product product;
 
     public long getDealId() {
@@ -36,6 +37,7 @@ public class Deal {
     }
 
     public void setProduct(Product product) {
+        this.product = product;
         if(product == null) { // 상품 자체 삭제 또는 상품 진행 단계 변경(거래완료 -> 예약중 or 거래완료 -> 판매중)으로 인한 연관관계 삭제
             this.product.setDeal(null); // 상품 - 거래 연관관계 삭제
             this.member.getDeals().remove(this); // 회원 - 거래 연관관계 삭제
@@ -43,6 +45,5 @@ public class Deal {
             this.product.setDeal(this);
             this.member.getDeals().add(this); // 만약 거래완료에서 예약중으로 바꾸면 deal 목록에서 빼야하는디 이건 더 생각해서 해결하기
         }
-        this.product = product;
     }
 }
