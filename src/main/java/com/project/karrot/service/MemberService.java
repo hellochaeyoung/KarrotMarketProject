0017a88393ec1b1925dd9367e9d661fe0428890a
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-02-18 02:52:48 +0900

    Entity -> DTO로 변경

--- src/main/java/com/project/karrot/service/MemberService.java
+++ src/main/java/com/project/karrot/service/MemberService.java
@@ -1,6 +1,10 @@
 package com.project.karrot.service;
 
+import com.project.karrot.domain.Location;
 import com.project.karrot.domain.Member;
+import com.project.karrot.dto.MemberRequestDto;
+import com.project.karrot.dto.MemberResponseDto;
+import com.project.karrot.repository.LocationRepository;
 import com.project.karrot.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,15 +17,21 @@ import java.util.Optional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final LocationRepository locationRepository;
 
     // 직접 new 해서 생성하는 게 아닌 외부에서 집어넣을 수 있게
-    public MemberService(MemberRepository memberRepository) {
+    public MemberService(MemberRepository memberRepository, LocationRepository locationRepository) {
         this.memberRepository = memberRepository;
+        this.locationRepository = locationRepository;
     }
 
-    public Long join(Member member) {
+    public Long join(MemberRequestDto memberRequestDto) {
 
-        validateDuplicateMember(member); // 닉네임 중복 회원 검증
+        validateDuplicateMember(memberRequestDto); // 닉네임 중복 회원 검증
+
+        Member member = memberRequestDto.toEntity();
+        Location findLocation = locationRepository.findByName(memberRequestDto.getLocation()).orElseThrow(NullPointerException::new).get(0);
+        member.setLocation(findLocation);
 
         memberRepository.save(member);
 
@@ -30,11 +40,10 @@ public class MemberService {
         return member.getId();
     }
 
-    // 이렇게 메소드로 빼주는 게 좋다.
-    private void validateDuplicateMember(Member member) {
+    private void validateDuplicateMember(MemberRequestDto memberRequestDto) {
         // findByNickName은 Optional을 리턴하는데 Optional을 써주는건 좋지않다.
         // 바로 메소드를 붙여 사용하는 게 더 좋은 방법이다.
-        memberRepository.findByNickName(member.getNickName())
+        memberRepository.findByNickName(memberRequestDto.getNickName())
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 닉네임입니다.");
                 });
@@ -52,13 +61,16 @@ public class MemberService {
         return memberRepository.findByNickName(nickName);
     }
 
-    public Member login(String email, String password) {
-        return memberRepository.findByEmail(email)
-                .filter(member -> member.getPassword().equals(password))
-                .orElse(null); }
+    public MemberResponseDto login(MemberRequestDto memberRequestDto) {
+        Member findMember = memberRepository.findByEmail(memberRequestDto.getEmail())
+                .filter(member -> member.getPassword().equals(memberRequestDto.getPassword()))
+                .orElse(null);
+        if(findMember == null) {
+            return null;
+        }else {
+            return new MemberResponseDto(findMember);
+        }
 
-    public List<Member> findMembers() {
-        return memberRepository.findAll();
     }
 
     public void remove(Member member) {
