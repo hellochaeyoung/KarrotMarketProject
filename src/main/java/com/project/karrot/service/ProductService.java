39ebb8806ddb2a989229c004d6d475a2aea74811
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-02-20 23:50:09 +0900

    Entity -> DTO로 수정

--- src/main/java/com/project/karrot/service/ProductService.java
+++ src/main/java/com/project/karrot/service/ProductService.java
@@ -3,7 +3,10 @@ package com.project.karrot.service;
 import com.project.karrot.domain.*;
 import com.project.karrot.dto.MemberRequestDto;
 import com.project.karrot.dto.ProductListResponseDto;
+import com.project.karrot.dto.ProductRequestDto;
 import com.project.karrot.dto.ProductResponseDto;
+import com.project.karrot.repository.CategoryRepository;
+import com.project.karrot.repository.MemberRepository;
 import com.project.karrot.repository.ProductRepository;
 
 import javax.transaction.Transactional;
@@ -12,19 +15,31 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Transactional
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+    private final MemberRepository memberRepository;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, CategoryRepository categoryRepository, MemberRepository memberRepository) {
         this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
+        this.memberRepository = memberRepository;
     }
 
-    public Product register(Product product) {
+    public Product register(ProductRequestDto productRequestDto, MemberRequestDto memberRequestDto) {
 
-        Product result = productRepository.save(product);
+        Member member = memberRepository.findByNickName(memberRequestDto.getNickName()).orElseThrow();
+        Category findCategory = categoryRepository.findByCategoryName(productRequestDto.getCategoryName()).orElseThrow();
+        String time = fomatDate();
+
+        productRequestDto.setProductRequestDto(member, findCategory, time);
+        Product newProduct = productRequestDto.toEntity();
+
+        Product result = productRepository.save(newProduct);
 
         productRepository.flush();
 
@@ -38,8 +53,8 @@ public class ProductService {
         return format.format(now);
     }
 
-    public ProductResponseDto find(Long productId) {
-        Product product = productRepository.findById(productId).orElseThrow(NullPointerException::new);
+    public ProductResponseDto findById(Long productId) {
+        Product product = productRepository.findById(productId).orElseThrow();
 
         return new ProductResponseDto(product);
     }
@@ -47,25 +62,33 @@ public class ProductService {
     public List<ProductResponseDto> findByLocation(Location location) {
         List<Product> productList = productRepository.findByLocationId(location.getLocationId()).orElseGet(ArrayList::new);
 
-        return new ProductListResponseDto().EntityListToDtoList(productList);
+        return productList.stream()
+                .map(ProductResponseDto::new)
+                .collect(Collectors.toList());
     }
 
     public List<ProductResponseDto> findByLocationAndCategory(Location location, Category category) {
         List<Product> productList = productRepository.findByLocationIdAndCategoryId(location.getLocationId(), category.getCategoryId()).orElseGet(ArrayList::new);
 
-        return new ProductListResponseDto().EntityListToDtoList(productList);
+        return productList.stream()
+                .map(ProductResponseDto::new)
+                .collect(Collectors.toList());
     }
 
     public List<ProductResponseDto> findByMember(Long memberId) {
         List<Product> productList = productRepository.findByMemberId(memberId).orElseGet(ArrayList::new);
 
-        return new ProductListResponseDto().EntityListToDtoList(productList);
+        return productList.stream()
+                .map(ProductResponseDto::new)
+                .collect(Collectors.toList());
     }
 
     public List<ProductResponseDto> findByMemberAndStatus(Long memberId, ProductStatus status) {
         List<Product> productList = productRepository.findByMemberIdAndProductStatus(memberId, status.name()).orElseGet(ArrayList::new);
 
-        return new ProductListResponseDto().EntityListToDtoList(productList);
+        return productList.stream()
+                .map(ProductResponseDto::new)
+                .collect(Collectors.toList());
     }
 
     public void remove(Product product) {
