407632d07bfccb158b0b2233fb7363d6f4bd43af
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-02-24 18:07:37 +0900

    관심상품 리스트 추가

--- src/main/java/com/project/karrot/dto/MemberResponseDto.java
+++ src/main/java/com/project/karrot/dto/MemberResponseDto.java
@@ -1,6 +1,7 @@
 package com.project.karrot.dto;
 
 import com.project.karrot.domain.Deal;
+import com.project.karrot.domain.InterestedProduct;
 import com.project.karrot.domain.Location;
 import com.project.karrot.domain.Member;
 import lombok.Getter;
@@ -15,6 +16,7 @@ public class MemberResponseDto {
     private final String nickName;
     private final Location location;
     private List<Deal> deals;
+    private List<InterestedProduct> interestedProducts;
 
     public MemberResponseDto(Member member) {
         this.id = member.getId();
@@ -22,6 +24,7 @@ public class MemberResponseDto {
         this.nickName = member.getNickName();
         this.location = member.getLocation();
         this.deals = member.getDeals();
+        this.interestedProducts = member.getInterestedProducts();
     }
 
 }
