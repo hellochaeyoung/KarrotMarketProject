e81ad99faf7e6170c0a71138799de67ff71e24c5
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-02-25 16:49:30 +0900

    Entity -> DTO 및 어노테이션 변경

--- src/main/java/com/project/karrot/controller/HomeController.java
+++ src/main/java/com/project/karrot/controller/HomeController.java
@@ -9,15 +9,22 @@ import com.project.karrot.service.CategoryService;
 import com.project.karrot.service.LocationService;
 import com.project.karrot.service.MemberService;
 import com.project.karrot.service.ProductService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/")
 public class HomeController {
 
     private final MemberService memberService;
@@ -25,15 +32,9 @@ public class HomeController {
     private final CategoryService categoryService;
     private final LocationService locationService;
 
-    public HomeController(MemberService memberService, ProductService productService, CategoryService categoryService, LocationService locationService) {
-        this.memberService = memberService;
-        this.productService = productService;
-        this.categoryService = categoryService;
-        this.locationService = locationService;
-    }
-
-    @GetMapping("/")
-    public String home(@SessionAttribute(name = SessionConstants.LOGIN_MEMBER, required = false) MemberRequestDto loginMember, Model model) {
+    @ApiOperation(value = "로그인 및 회원가입 페이지", notes = "페이지 접속 시 처음 보여지는 화면 입니다.")
+    @GetMapping()
+    public String home(MemberRequestDto loginMember, Model model) {
 
         if(loginMember == null) {
             return "home";
@@ -42,10 +43,10 @@ public class HomeController {
         MemberResponseDto memberResponseDto = memberService.findByNickName(loginMember.getNickName());
         model.addAttribute("member", memberResponseDto);
 
-        List<ProductResponseDto> products = productService.findByLocation(memberResponseDto.getLocation());
+        List<ProductResponseDto> products = productService.findByLocation(memberResponseDto.getLocation().getLocationId());
         model.addAttribute("products", products);
 
-        List<Category> allCategory = categoryService.findAll();
+        List<CategoryResponseDto> allCategory = categoryService.findAll();
         model.addAttribute("allCategory", allCategory);
 
         return "mains/mainPage";
