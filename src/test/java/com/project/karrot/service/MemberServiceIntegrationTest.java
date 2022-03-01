b1902d5612e2c3d09f58d0b25c95d146142eff14
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-03-02 02:34:17 +0900

    비밀번호 암호화 기능 추가 후 회원가입 테스트

--- src/test/java/com/project/karrot/service/MemberServiceIntegrationTest.java
+++ src/test/java/com/project/karrot/service/MemberServiceIntegrationTest.java
@@ -3,15 +3,17 @@ package com.project.karrot.service;
 import com.project.karrot.src.member.Member;
 import com.project.karrot.src.member.MemberRepository;
 import com.project.karrot.src.member.MemberService;
+import com.project.karrot.src.member.dto.MemberRequestDto;
+import com.project.karrot.src.member.dto.MemberResponseDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.transaction.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -19,8 +21,35 @@ class MemberServiceIntegrationTest {
 
     @Autowired
     MemberService memberService;
-    @Autowired MemberRepository memberRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
+    @Test
+    public void 회원가입() {
+        MemberRequestDto memberReq = new MemberRequestDto();
+
+        String email = "cyahn@gmail.com";
+
+        memberReq.setName("chaeyoung");
+        memberReq.setNickName("cyyyyyy");
+        memberReq.setEmail(email);
+        memberReq.setLocationName("경기도 부천시 상동");
+        memberReq.setPassword("aaaa");
+        memberReq.setPhoneNumber("010-1111-1111");
+
+        memberService.join(memberReq);
+        MemberResponseDto memberRes = memberService.findByEmail(email);
+
+        String encodedPassword = memberRes.getPassword();
+        System.out.println(memberRes.getId());
+        //System.out.println(memberRes.getAddress());
+        System.out.println(memberRes.getNickName());
+
+        assertTrue(passwordEncoder.matches("aaaa", encodedPassword));
+
+    }
+
+/*
     @Test
     public void 회원가입() throws Exception {
         //Given
@@ -58,5 +87,7 @@ class MemberServiceIntegrationTest {
         //Then
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 닉네임입니다.");
     }
+
+ */
 
 }
