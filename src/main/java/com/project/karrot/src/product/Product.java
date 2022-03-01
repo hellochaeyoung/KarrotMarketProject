0725df1f65f9af1246f64b0aa9c20ce50ca12bd3
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-03-01 16:00:41 +0900

    ProductImageFile import 추가

--- src/main/java/com/project/karrot/src/product/Product.java
+++ src/main/java/com/project/karrot/src/product/Product.java
@@ -4,6 +4,7 @@ import com.project.karrot.src.ProductStatus;
 import com.project.karrot.src.category.Category;
 import com.project.karrot.src.comment.Comment;
 import com.project.karrot.src.deal.Deal;
+import com.project.karrot.src.image.ProductImageFile;
 import com.project.karrot.src.location.Location;
 import com.project.karrot.src.member.Member;
 import lombok.Builder;
@@ -14,7 +15,7 @@ import java.util.List;
 
 @Builder
 @Entity
-public class Product<ProductImageFile> {
+public class Product {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long productId;
