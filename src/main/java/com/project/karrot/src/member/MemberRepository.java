d4c1632694653ab6a4e98577846eaddc83bbe154
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-03-03 02:28:25 +0900

    authority와 member 동시조회 메소드 구현

--- src/main/java/com/project/karrot/src/member/MemberRepository.java
+++ src/main/java/com/project/karrot/src/member/MemberRepository.java
@@ -1,10 +1,14 @@
 package com.project.karrot.src.member;
 
 import com.project.karrot.src.member.Member;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    @EntityGraph(attributePaths = "authorities")
+    Optional<Member> findOneWithAuthoritiesByEmail(String email);
 
     Optional<Member> findByName(String name);
 
