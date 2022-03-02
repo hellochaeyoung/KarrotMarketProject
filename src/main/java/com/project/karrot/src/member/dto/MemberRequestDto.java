b7bf43a5d03c38fa6853d587621cb40b055d872a
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-03-03 02:28:59 +0900

    인증 필요 정보 추가

--- src/main/java/com/project/karrot/src/member/dto/MemberRequestDto.java
+++ src/main/java/com/project/karrot/src/member/dto/MemberRequestDto.java
@@ -1,9 +1,13 @@
 package com.project.karrot.src.member.dto;
 
-import com.project.karrot.src.auth.Salt;
 import com.project.karrot.src.location.Location;
+import com.project.karrot.src.member.Authority;
 import com.project.karrot.src.member.Member;
 import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Collections;
 
 @Getter
 @Setter
@@ -13,32 +17,49 @@ import lombok.*;
 public class MemberRequestDto {
 
     private Long memberId;
+
+    @NotNull
+    @Size(min = 3, max = 50)
     private String name;
+
+    @NotNull
+    @Size(min = 10, max = 50)
     private String email;
+
+    @NotNull
+    @Size(min = 3, max = 50)
     private String password;
+
     private String phoneNumber;
+
+    @NotNull
+    @Size(min = 1, max = 50)
     private String nickName;
+
     private String locationName;
 
-    private Location location;
-
-    public Member toEntity() {
+    public Member toEntity(String password, Authority authority) {
         return Member.builder()
                 .name(name)
                 .email(email)
                 .password(password)
                 .phoneNumber(phoneNumber)
                 .nickName(nickName)
+                .authorities(Collections.singleton(authority))
+                .activated(true)
                 //.location(location)
                 .build();
     }
-
+/*
     public void setMemberRequestDto(Location location) {
         this.location = location;
     }
 
     public void setMemberAuth(String encodePassword) {
         this.password = encodePassword;
+
     }
+
+ */
 
 }
