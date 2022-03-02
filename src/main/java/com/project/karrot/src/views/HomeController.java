bbb9b38f900739ea290623561bb977bf78a039c2
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-03-03 02:26:42 +0900

    회원가입 기능 수정

--- src/main/java/com/project/karrot/src/views/HomeController.java
+++ src/main/java/com/project/karrot/src/views/HomeController.java
@@ -1,8 +1,10 @@
 package com.project.karrot.src.views;
 
+import com.project.karrot.common.Api.Response;
 import com.project.karrot.src.category.CategoryService;
 import com.project.karrot.src.category.dto.CategoryResponseDto;
 import com.project.karrot.src.location.LocationService;
+import com.project.karrot.src.location.dto.LocationResponseDto;
 import com.project.karrot.src.member.MemberService;
 import com.project.karrot.src.member.dto.MemberRequestDto;
 import com.project.karrot.src.member.dto.MemberResponseDto;
@@ -10,15 +12,16 @@ import com.project.karrot.src.product.ProductService;
 import com.project.karrot.src.product.dto.ProductResponseDto;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/home")
 public class HomeController {
 
     private final MemberService memberService;
@@ -27,23 +30,26 @@ public class HomeController {
     private final LocationService locationService;
 
     @ApiOperation(value = "로그인 및 회원가입 페이지", notes = "페이지 접속 시 처음 보여지는 화면 입니다.")
-    @GetMapping("/")
-    public String home(MemberRequestDto loginMember, Model model) {
-
-        if(loginMember == null) {
-            return "home";
-        }
-
-        MemberResponseDto memberResponseDto = memberService.findByNickName(loginMember.getNickName());
-        model.addAttribute("member", memberResponseDto);
-
-        List<ProductResponseDto> products = productService.findByLocation(memberResponseDto.getLocation().getLocationId());
-        model.addAttribute("products", products);
-
-        List<CategoryResponseDto> allCategory = categoryService.findAll();
-        model.addAttribute("allCategory", allCategory);
-
-        return "mains/mainPage";
+    @GetMapping()
+    public ResponseEntity<?> home() {
+        return Response.success("hello", HttpStatus.OK).reponseBuild();
     }
+
+    @ApiOperation(value = "회원 가입 - 지역 조회", notes = "회원 가입 시 거주 지역을 검색한다")
+    @GetMapping("/signUp/{address}")
+    public List<LocationResponseDto> getAll(@PathVariable String address) {
+
+        return locationService.findByAddressAll(address);
+    }
+
+    @ApiOperation(value = "회원 가입", notes = "회원 가입을 한다.")
+    @PostMapping("/signUp")
+    public ResponseEntity<?> create(@RequestBody MemberRequestDto memberRequestDto) {
+        ;
+        // 회원가입 예외처리 추가 필요
+
+        return new ResponseEntity<>(memberService.join(memberRequestDto), HttpStatus.OK);
+    }
+
 }
 
