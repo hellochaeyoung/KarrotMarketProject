7297f1f459cfdbb38c8ea6fce8b35fc37e86902e
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-03-03 02:27:10 +0900

    로그인 전용 DTO 구현

---
+++ src/main/java/com/project/karrot/src/member/dto/LoginReqDto.java
@@ -0,0 +1,22 @@
+package com.project.karrot.src.member.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginReqDto {
+
+    @NotNull
+    @Size(min = 10, max = 50)
+    private String email;
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String password;
+}
---
+++ src/main/java/com/project/karrot/src/member/dto/TokenResDto.java
@@ -0,0 +1,14 @@
+package com.project.karrot.src.member.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenResDto {
+
+    private String token;
+
+}
