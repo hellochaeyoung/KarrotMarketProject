52f8fcae5b3b78ac9ca05c1d5d0007c6aa234c52
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-03-08 03:05:42 +0900

    로그인 인증 후 확인 구현

--- src/main/java/com/project/karrot/src/member/AuthController.java
+++ src/main/java/com/project/karrot/src/member/AuthController.java
@@ -5,6 +5,7 @@ import com.project.karrot.src.jwt.JwtFilter;
 import com.project.karrot.src.jwt.TokenProvider;
 import com.project.karrot.src.member.dto.LoginReqDto;
 import com.project.karrot.src.member.dto.TokenResDto;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -13,10 +14,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -27,25 +25,29 @@ public class AuthController {
 
     private final TokenProvider tokenProvider;
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final MemberService memberService;
 
-    @PostMapping("/authenticate")
+    @ApiOperation(value = "로그인 - 인증", notes = "이메일과 비밀번호가 일치하는지 확인한다.")
+    @PostMapping("/auth")
     public ResponseEntity<TokenResDto> authorize(@RequestBody LoginReqDto loginReqDto) {
 
-        System.out.println("authorize method access");
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(loginReqDto.getEmail(), loginReqDto.getPassword());
 
-        System.out.println("authorize method access");
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        System.out.println("authorize method access");
         String jwt = tokenProvider.createToken(authentication);
 
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
-        System.out.println("authorize method access");
 
         return new ResponseEntity<>(new TokenResDto(jwt), httpHeaders, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "로그인 - 완료", notes = "인증 되면 로그인을 성공하고 완료한다.")
+    @GetMapping("/user")
+    public ResponseEntity<?> finish() {
+        return ResponseEntity.ok(memberService.login());
     }
 }
