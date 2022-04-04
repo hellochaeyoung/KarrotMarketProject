6d7e9c39b40a6171362c7d2fe6970929da133415
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-04-05 02:37:06 +0900

    JPA type missing 에러로 인한 query 생성 불가 오류 해결을 위해 request 수정

--- src/main/java/com/project/karrot/src/views/MainController.java
+++ src/main/java/com/project/karrot/src/views/MainController.java
@@ -7,6 +7,8 @@ import com.project.karrot.src.category.dto.CategoryRequestDto;
 import com.project.karrot.src.category.dto.CategoryResponseDto;
 import com.project.karrot.src.location.dto.LocationRequestDto;
 import com.project.karrot.src.member.Member;
+import com.project.karrot.src.member.MemberService;
+import com.project.karrot.src.member.dto.MemberResponseDto;
 import com.project.karrot.src.product.ProductService;
 import com.project.karrot.src.product.dto.ProductAndCategoryRes;
 import com.project.karrot.src.product.dto.ProductRequestDto;
@@ -22,22 +24,24 @@ import java.util.List;
 @RequestMapping("/main")
 public class MainController {
 
+    private final MemberService memberService;
     private final ProductService productService;
     private final CategoryService categoryService;
 
     @ApiOperation(value = "메인 화면 조회", notes = "거주 지역과 설정한 카테고리에 해당하는 상품들을 조회한다.")
     @PostMapping("/")
     @LoginCheck
-    public ProductAndCategoryRes setCategory(@RequestBody LocationRequestDto memberLocation, @RequestBody CategoryRequestDto categoryRequest) {
+    public ProductAndCategoryRes setCategory(@CurrentMemberId Long memberId, @RequestBody CategoryRequestDto categoryRequest) {
 
+        MemberResponseDto loginMember = memberService.find(memberId);
         List<CategoryResponseDto> categoryList = categoryService.findAll();
-        List<ProductResponseDto> productList = productService.findByLocationAndCategory(memberLocation.getLocationId(), categoryRequest.getCategoryId());
+        List<ProductResponseDto> productList = productService.findByLocationAndCategory(loginMember.getLocationId(), categoryRequest.getCategoryId());
 
         return new ProductAndCategoryRes(categoryList, productList);
     }
 
 
-    @ApiOperation(value = "상품 등록 화면 조회", notes = "카테고리 목록을 세팅한다.")
+    @ApiOperation(value = "상품 등록 화면 조회", notes = "카테고리 목록 데이터를 가져와 보여준다.")
     @GetMapping("/products/new")
     @LoginCheck
     public List<CategoryResponseDto> productRegisterForm() {
