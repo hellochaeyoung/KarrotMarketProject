fe9b194233aa3bcfdd3d523bbd0454360e8fa3dc
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-04-06 15:16:12 +0900

    fetchType LAZY 적용

--- src/main/java/com/project/karrot/src/interest/InterestedProduct.java
+++ src/main/java/com/project/karrot/src/interest/InterestedProduct.java
@@ -16,7 +16,7 @@ public class InterestedProduct {
     @Column(name = "interested_id")
     private Long id;
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
