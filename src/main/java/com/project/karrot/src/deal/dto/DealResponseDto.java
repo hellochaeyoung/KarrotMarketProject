5855231d2abb0a1937c819b035f3ff2ea8c88b49
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-04-08 16:23:23 +0900

    productId -> productResponseDto로 변경

--- src/main/java/com/project/karrot/src/deal/dto/DealResponseDto.java
+++ src/main/java/com/project/karrot/src/deal/dto/DealResponseDto.java
@@ -1,6 +1,8 @@
 package com.project.karrot.src.deal.dto;
 
 import com.project.karrot.src.deal.Deal;
+import com.project.karrot.src.product.Product;
+import com.project.karrot.src.product.dto.ProductResponseDto;
 import lombok.*;
 
 @Getter
@@ -10,10 +12,11 @@ import lombok.*;
 public class DealResponseDto {
 
     private Long dealId;
-    private Long productId;
+    private ProductResponseDto productResponseDto;
 
     public DealResponseDto(Deal deal) {
         this.dealId = deal.getId();
-        this.productId = deal.getProduct().getId();
+        productResponseDto = new ProductResponseDto(deal.getProduct());
     }
+
 }
