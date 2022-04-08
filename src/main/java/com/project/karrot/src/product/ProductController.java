2f6ccc0e0df7d2745c0da809d4720e84a730cf13
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-04-08 16:32:11 +0900

    @LoginCheck 적용 및 좋아요 기능 구현, 댓글 등록 기능 구현

--- src/main/java/com/project/karrot/src/product/ProductController.java
+++ src/main/java/com/project/karrot/src/product/ProductController.java
@@ -1,13 +1,22 @@
 package com.project.karrot.src.product;
 
+import com.project.karrot.src.annotation.CurrentMemberId;
+import com.project.karrot.src.annotation.LoginCheck;
 import com.project.karrot.src.comment.CommentService;
 import com.project.karrot.src.comment.dto.CommentRequestDto;
 import com.project.karrot.src.comment.dto.CommentResponseDto;
+import com.project.karrot.src.interest.InterestedService;
+import com.project.karrot.src.interest.dto.InterestedRequestDto;
+import com.project.karrot.src.interest.dto.InterestedResponseDto;
 import com.project.karrot.src.member.MemberService;
 import com.project.karrot.src.member.dto.MemberResponseDto;
+import com.project.karrot.src.product.dto.ProductListResponseDto;
+import com.project.karrot.src.product.dto.ProductRequestDto;
 import com.project.karrot.src.product.dto.ProductResponseDto;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
@@ -16,47 +25,40 @@ import java.util.*;
 @RequestMapping("/products")
 public class ProductController {
 
-    private final MemberService memberService;
     private final ProductService productService;
     private final CommentService commentService;
+    private final InterestedService interestedService;
 
-    @ApiOperation(value = "상품 상세 조회", notes = "상품을 조회한다.")
+    @ApiOperation(value = "상품 상세 조회", notes = "상품 내용 상세 조회 및 등록자의 다른 상품들까지 조회한다.")
     @GetMapping("/{productId}")
-    public List<ProductResponseDto> viewProduct(@PathVariable Long productId) {
+    @LoginCheck
+    public ResponseEntity<?> viewProduct(@PathVariable Long productId, @RequestParam Long registerMemberId) {
+        ProductResponseDto view = productService.findById(productId);
+        List<ProductResponseDto> otherList = productService.findByMemberId(registerMemberId);
+        return new ResponseEntity<>(new ProductListResponseDto(view, otherList), HttpStatus.OK);
+    }
 
-        ProductResponseDto productResponseDto = productService.findById(productId);
-
-        MemberResponseDto productOwner = memberService.find(productResponseDto.getMemberId());
-        List<ProductResponseDto> productAllList = productService.findByMemberId(productOwner.getId());
-        productAllList.remove(productResponseDto);
-
-        return productAllList;
+    @ApiOperation(value = "상품 좋아요 누르기", notes = "상품 좋아요 버튼을 눌러 관심 목록에 추가한다.")
+    @PutMapping("/{productId}")
+    @LoginCheck
+    public ResponseEntity<?> like(@RequestBody InterestedRequestDto interestedRequestDto) {
+        InterestedResponseDto interestedResponseDto = interestedService.add(interestedRequestDto);
+        return new ResponseEntity<>(interestedResponseDto.getProductResponseDto().getLikeCount(), HttpStatus.OK);
     }
 
 
     @ApiOperation(value = "상품 댓글 조회", notes = "상품에 등록된 댓글을 조회한다.")
     @GetMapping("/{productId}/comments")
-    public List<CommentResponseDto> viewComment(@PathVariable Long productId) {
-
-        return commentService.findByProductId(productId);
-
+    @LoginCheck
+    public ResponseEntity<?> viewComment(@PathVariable Long productId) {
+        return new ResponseEntity<>(commentService.findByProductId(productId), HttpStatus.OK);
     }
 
     @ApiOperation(value = "상품 댓글 등록", notes = "상품에 댓글을 등록한다.")
     @PostMapping("/{productId}/comments")
-    public CommentResponseDto registerComment(@RequestBody CommentRequestDto commentRequestDto,
+    @LoginCheck
+    public ResponseEntity<?> registerComment(@CurrentMemberId Long memberId, @RequestBody CommentRequestDto commentRequestDto,
                                   @PathVariable Long productId) {
-
-        ProductResponseDto productResponseDto = productService.findById(productId);
-
-        MemberResponseDto memberResponseDto = memberService.find(productResponseDto.getMemberId());
-
-        //수정 필요
-        //comment.setContents(commentForm.getContents());
-        //comment.setMember(loginMember);
-        //comment.setProduct(product);
-
-        return commentService.register(commentRequestDto, memberResponseDto);
-
+        return new ResponseEntity<>(commentService.register(commentRequestDto, memberId, productId), HttpStatus.OK);
     }
 }
