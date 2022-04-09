e98b541540ec2dcd87bbe7b5a3f237b88dfad34c
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-04-09 14:45:02 +0900

    메인 화면 조회 DTO 변경 및 지역 검색 api 구현

--- src/main/java/com/project/karrot/src/views/MainController.java
+++ src/main/java/com/project/karrot/src/views/MainController.java
@@ -3,8 +3,10 @@ package com.project.karrot.src.views;
 import com.project.karrot.src.annotation.CurrentMemberId;
 import com.project.karrot.src.annotation.LoginCheck;
 import com.project.karrot.src.category.CategoryService;
+import com.project.karrot.src.category.dto.CategoryAndLocationRequestDto;
 import com.project.karrot.src.category.dto.CategoryRequestDto;
 import com.project.karrot.src.category.dto.CategoryResponseDto;
+import com.project.karrot.src.location.LocationService;
 import com.project.karrot.src.location.dto.LocationRequestDto;
 import com.project.karrot.src.member.Member;
 import com.project.karrot.src.member.MemberService;
@@ -30,17 +32,24 @@ public class MainController {
     private final MemberService memberService;
     private final ProductService productService;
     private final CategoryService categoryService;
+    private final LocationService locationService;
 
-    @ApiOperation(value = "메인 화면 조회", notes = "거주 지역과 설정한 카테고리에 해당하는 상품들을 조회한다.")
+    @ApiOperation(value = "메인 화면 조회", notes = "지역과 설정한 카테고리에 해당하는 상품들을 조회한다.")
     @PostMapping("/")
     @LoginCheck
-    public ResponseEntity<?> setCategory(@CurrentMemberId Long memberId, @RequestBody CategoryRequestDto categoryRequest) {
+    public ResponseEntity<?> viewProduct(@CurrentMemberId Long memberId, @RequestBody CategoryAndLocationRequestDto categoryAndLocationRequestDto) {
 
-        MemberResponseDto loginMember = memberService.find(memberId);
         List<CategoryResponseDto> categoryList = categoryService.findAll();
-        List<ProductResponseDto> productList = productService.findByLocationAndCategory(loginMember.getLocationId(), categoryRequest.getCategoryId());
+        List<ProductResponseDto> productList = productService.findByLocationAndCategory(categoryAndLocationRequestDto.getLocationId(), categoryAndLocationRequestDto.getCategoryId());
 
         return new ResponseEntity<>(new ProductAndCategoryRes(categoryList, productList), HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "메인 화면 - 지역 검색", notes = "지역을 검색한다")
+    @PostMapping("/location")
+    @LoginCheck
+    public ResponseEntity<?> getLocation(@RequestBody String locationName) {
+        return new ResponseEntity<>(locationService.findByAddressAll(locationName), HttpStatus.OK);
     }
 
 
@@ -48,7 +57,6 @@ public class MainController {
     @GetMapping("/products/new")
     @LoginCheck
     public ResponseEntity<?> productRegisterForm() {
-
         return new ResponseEntity<>(categoryService.findAll(), HttpStatus.OK);
     }
 
@@ -57,9 +65,7 @@ public class MainController {
     @PostMapping("/products/new")
     @LoginCheck
     public ResponseEntity<?> register(@CurrentMemberId Long memberId, @RequestBody ProductRequestDto productRequestDto) {
-
         productRequestDto.setMemberId(memberId); // 추후에 수정할 것
-
         return new ResponseEntity<>(productService.register(productRequestDto), HttpStatus.OK);
     }
 
