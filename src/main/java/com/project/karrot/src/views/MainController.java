ee60a91eedcb551397346efa36a0c3d8850fb3c7
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-04-12 03:35:28 +0900

    상품 등록 시 이미지 업로드 기능 추가 구현

--- src/main/java/com/project/karrot/src/views/MainController.java
+++ src/main/java/com/project/karrot/src/views/MainController.java
@@ -6,21 +6,25 @@ import com.project.karrot.src.category.CategoryService;
 import com.project.karrot.src.category.dto.CategoryAndLocationRequestDto;
 import com.project.karrot.src.category.dto.CategoryRequestDto;
 import com.project.karrot.src.category.dto.CategoryResponseDto;
+import com.project.karrot.src.image.FileUploadService;
 import com.project.karrot.src.location.LocationService;
 import com.project.karrot.src.location.dto.LocationRequestDto;
 import com.project.karrot.src.member.Member;
 import com.project.karrot.src.member.MemberService;
 import com.project.karrot.src.member.dto.MemberResponseDto;
 import com.project.karrot.src.product.ProductService;
-import com.project.karrot.src.product.dto.ProductAndCategoryRes;
-import com.project.karrot.src.product.dto.ProductRequestDto;
-import com.project.karrot.src.product.dto.ProductResponseDto;
+import com.project.karrot.src.product.dto.*;
+import com.project.karrot.src.productimage.ProductImageService;
+import com.project.karrot.src.productimage.dto.ProductImageRequestDto;
 import io.jsonwebtoken.lang.Collections;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -33,6 +37,8 @@ public class MainController {
     private final ProductService productService;
     private final CategoryService categoryService;
     private final LocationService locationService;
+    private final ProductImageService productImageService;
+    private final FileUploadService fileUploadService;
 
     @ApiOperation(value = "메인 화면 조회", notes = "지역과 설정한 카테고리에 해당하는 상품들을 조회한다.")
     @PostMapping("/")
@@ -64,9 +70,18 @@ public class MainController {
     @ApiOperation(value = "상품 등록", notes = "새 상품을 등록한다.")
     @PostMapping("/products/new")
     @LoginCheck
-    public ResponseEntity<?> register(@CurrentMemberId Long memberId, @RequestBody ProductRequestDto productRequestDto) {
-        productRequestDto.setMemberId(memberId); // 추후에 수정할 것
-        return new ResponseEntity<>(productService.register(productRequestDto), HttpStatus.OK);
+    public ResponseEntity<?> register(@CurrentMemberId Long memberId, @RequestPart ProductRequestDto productRequestDto, @RequestPart(required = false) List<MultipartFile> fileList) {
+
+        List<String> fileUrlList = new ArrayList<>();
+        fileList.listIterator().forEachRemaining(file -> {
+            String url = fileUploadService.uploadImage(file);
+            fileUrlList.add(url);
+        });
+
+        productRequestDto.toReady(memberId, fileUrlList);
+        ProductAndImageResponseDto productAndImageResponseDto = productService.register(productRequestDto);
+
+        return new ResponseEntity<>(productAndImageResponseDto, HttpStatus.OK);
     }
 
 }
