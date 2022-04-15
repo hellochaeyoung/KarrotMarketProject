a29388af814ed41de872fe08aaecc55c425d3762
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-04-16 01:00:59 +0900

    CommentService 테스트 코드 작성 - 댓글 등록,수정,삭제

--- src/test/java/com/project/karrot/service/CommentServiceTest.java
+++ src/test/java/com/project/karrot/service/CommentServiceTest.java
@@ -1,20 +1,22 @@
 package com.project.karrot.service;
 
-import com.project.karrot.src.comment.Comment;
 import com.project.karrot.src.comment.CommentService;
-import com.project.karrot.src.member.Member;
+import com.project.karrot.src.comment.dto.CommentRequestDto;
+import com.project.karrot.src.comment.dto.CommentResponseDto;
 import com.project.karrot.src.member.MemberService;
-import com.project.karrot.src.product.Product;
+import com.project.karrot.src.member.dto.MemberResponseDto;
 import com.project.karrot.src.product.ProductService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.NoSuchElementException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @Transactional
@@ -22,63 +24,63 @@ public class CommentServiceTest {
 
     @Autowired
     MemberService memberService;
+
     @Autowired
     ProductService productService;
+
     @Autowired
     CommentService commentService;
 
-    @Test
-    public void 댓글_등록() {
-        Member member = new Member();
-        memberService.join(member);
+    private Long memberId = 9L;
+    private Long productId = 3L;
+    private CommentRequestDto commentRequestDto;
+    private CommentResponseDto commentResponseDto;
 
-        Product product = new Product();
-        product.setProductName("아이패드");
-        product.setContents("중고 아이패드 팝니다.");
-        product.setMember(member);
+    @BeforeEach
+    void init() {
 
-        Comment comment = new Comment();
-        comment.setContents("제가 사고싶어요!");
-        comment.setMember(member);
+        memberId = 9L;
 
-        Product registerProduct = productService.register(product);
-        comment.setProduct(product);
-        Comment comment1 = commentService.register(comment);
+        commentRequestDto = CommentRequestDto.builder()
+                .contents("제가 사고싶어요!")
+                .build();
 
-        Comment findComment = commentService.find(comment.getCommentId()).get();
+        commentResponseDto = commentService.register(commentRequestDto, memberId, productId);
 
-        assertEquals(comment1.getContents(), registerProduct.getComments().get(0).getContents());
-        assertEquals(registerProduct.getComments().get(0).getCommentId(), findComment.getCommentId());
+        commentRequestDto.setCommentId(commentResponseDto.getCommentId()); // 요청 dto로 이미 등록된 댓글 정보가 들어왔다 가정하기 위해 설정
 
     }
 
     @Test
-    public void 댓글_삭제() {
+     void 댓글_등록() {
 
-        Member member = new Member();
-        memberService.join(member);
+        Long registeredId = commentResponseDto.getProductId();
+        MemberResponseDto member = memberService.findByNickName(commentResponseDto.getNickName());
 
-        Product product = new Product();
-        product.setProductName("아이패드");
-        product.setContents("중고 아이패드 팝니다.");
-        product.setMember(member);
-
-        Comment comment = new Comment();
-        comment.setContents("제가 사고싶어요!");
-        comment.setMember(member);
-
-        Product registerProduct = productService.register(product);
-        comment.setProduct(registerProduct);
-        Comment registerComment = commentService.register(comment);
-
-        Comment findComment = commentService.find(registerComment.getCommentId()).get();
-
-        //assertEquals(registerProduct.getComments().size(), 1);
-
-        findComment.setProduct(null); // 연관관계 삭제
-        commentService.remove(findComment);
-
-        assertEquals(registerProduct.getComments().size(), 0);
+        assertEquals(productId, registeredId);
+        assertEquals(memberId, member.getId());
 
     }
+
+    @Test
+    void 댓글_수정() {
+
+        String content = "제가 정말정말 사고싶어요!";
+        CommentRequestDto requestDto = CommentRequestDto.builder().commentId(1L).contents(content).build();
+        String updateContent = commentService.update(requestDto, memberId, productId);
+
+        assertEquals(updateContent, content);
+    }
+
+    @Test
+    void 댓글_삭제() {
+
+        commentService.remove(commentRequestDto.getCommentId());
+
+        Assertions.assertThatThrownBy(() ->
+                commentService.find(commentRequestDto.getCommentId())).isInstanceOf(NoSuchElementException.class);
+
+
+    }
+
 }
