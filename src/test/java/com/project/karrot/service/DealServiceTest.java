089be9bb54c8ee99f8c4e768c97332cd21ad7952
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-04-16 01:02:11 +0900

    DealService 테스트 코드 작성 - 거래 조회(회원, 상품)

--- src/test/java/com/project/karrot/service/DealServiceTest.java
+++ src/test/java/com/project/karrot/service/DealServiceTest.java
@@ -1,91 +1,60 @@
 package com.project.karrot.service;
 
-import com.project.karrot.src.deal.Deal;
 import com.project.karrot.src.deal.DealService;
-import com.project.karrot.src.member.Member;
-import com.project.karrot.src.member.MemberService;
-import com.project.karrot.src.product.Product;
-import com.project.karrot.src.ProductStatus;
+import com.project.karrot.src.deal.dto.DealRequestDto;
+import com.project.karrot.src.deal.dto.DealResponseDto;
 import com.project.karrot.src.product.ProductService;
+import com.project.karrot.src.product.dto.ProductResponseDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
-
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @Transactional
 public class DealServiceTest {
 
     @Autowired
-    MemberService memberService;
-    @Autowired
     ProductService productService;
     @Autowired
     DealService dealService;
 
-    @Test
-    public void 거래완료_수정_테스트() {
+    private DealRequestDto dealRequestDto;
+    private DealResponseDto dealResponseDto;
 
-        Member member = new Member();
-        member.setName("cyahn");
-
-        Product product = new Product();
-        product.setMember(member);
-        product.setProductName("가방");
-
-        Long memberId = memberService.join(member);
-        Product product1 = productService.register(product);
-
-        product1.setProductStatus(ProductStatus.COMPLETE);
-        Deal deal = new Deal();
-        deal.setMember(member);
-        deal.setProduct(product1);
-
-        Deal registerDeal = dealService.register(deal);
-
-        assertEquals(member.getId(), registerDeal.getMember().getId());
-
-        product1.setProductStatus(ProductStatus.SALE);
-
-        assertEquals(registerDeal.getProduct(), null);
-        assertEquals(registerDeal.getMember().getDeals().size(), 0);
-
-        Deal removeDeal = dealService.register(registerDeal);
-
-        assertEquals(registerDeal.getDealId(), removeDeal.getDealId());
-
+    @BeforeEach
+    void init() {
+        dealRequestDto = DealRequestDto.builder().memberId(9L).productId(3L).build();
+        dealResponseDto = dealService.register(dealRequestDto);
     }
 
     @Test
-    public void 회원_거래완료_목록_조회() {
-
-        Member member = new Member();
-        member.setName("cyahn");
-
-        Product product = new Product();
-        product.setMember(member);
-        product.setProductName("가방");
-
-        Long memberId = memberService.join(member);
-        Product product1 = productService.register(product);
-
-        product1.setProductStatus(ProductStatus.COMPLETE);
-        Deal deal = new Deal();
-        deal.setMember(member);
-        deal.setProduct(product1);
-
-        Deal registerDeal = dealService.register(deal);
-
-        List<Deal> result = dealService.findByMember(member).orElseGet(() -> new ArrayList<Deal>());
-
-        assertEquals(result.get(0).getMember().getId(), memberId);
+    void 거래완료() {
+        assertEquals(dealRequestDto.getProductId(), dealResponseDto.getProductResponseDto().getProductId());
     }
+    @Test
+    public void 거래완료_회원조회() {
+
+        List<DealResponseDto> resultList = dealService.findByMember(dealRequestDto.getMemberId());
+
+        assertThat(resultList.size()).isGreaterThanOrEqualTo(0);
+    }
+
+    @Test
+    public void 거래완료_상품조회() {
+
+        DealResponseDto dealResponseDto = dealService.findByProduct(dealRequestDto.getProductId());
+
+        ProductResponseDto productResponseDto = productService.findById(dealRequestDto.getProductId());
+
+        assertEquals(dealResponseDto.getProductResponseDto().getProductName(), productResponseDto.getProductName());
+    }
+
+
 }
