42ea11b89de1d651440410c4da3bb34350af96b8
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-04-18 22:51:04 +0900

    생성자 - 롬복 어노테이션 적용으로 변경

--- src/main/java/com/project/karrot/src/product/dto/ProductAndImageResponseDto.java
+++ src/main/java/com/project/karrot/src/product/dto/ProductAndImageResponseDto.java
@@ -1,6 +1,7 @@
 package com.project.karrot.src.product.dto;
 
 import com.project.karrot.src.productimage.dto.ProductImageDto;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,13 +11,10 @@ import java.util.List;
 @Builder
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class ProductAndImageResponseDto {
 
     private ProductResponseDto productResponseDto;
     private List<ProductImageDto> imageFileList;
 
-    public ProductAndImageResponseDto(ProductResponseDto productResponseDto, List<ProductImageDto> imageFileList) {
-        this.productResponseDto = productResponseDto;
-        this.imageFileList = imageFileList;
-    }
 }
