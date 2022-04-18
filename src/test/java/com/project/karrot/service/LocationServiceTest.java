f526275aa91fea15428b6d139dc103fa991e70ff
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-04-18 23:53:29 +0900

    지역 기능 서비스 테스트 코드 작성 - 조회, 검색

--- src/test/java/com/project/karrot/service/LocationServiceTest.java
+++ src/test/java/com/project/karrot/service/LocationServiceTest.java
@@ -1,14 +1,16 @@
 package com.project.karrot.service;
 
-import com.project.karrot.src.location.Location;
 import com.project.karrot.src.location.LocationService;
+import com.project.karrot.src.location.dto.LocationResponseDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
-import java.util.ArrayList;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Transactional
@@ -17,15 +19,34 @@ public class LocationServiceTest {
     @Autowired
     LocationService locationService;
 
+    private Long locationId = 2L;
+
     @Test
-    public void 지역_검색() {
+    void 지역_조회() {
+        LocationResponseDto locationResponseDto = locationService.find(locationId);
 
-        String location1 = "상동";
-
-        List<Location> results = locationService.findByName(location1).orElseGet(() -> new ArrayList<Location>());
-
-        for(Location l : results) {
-            System.out.println(l.getAddress());
-        }
+        assertEquals(locationResponseDto.getAddress(), "경기도 부천시");
     }
+
+    @Test
+    void 지역_검색() {
+
+        String location1 = "서울특별시";
+
+        LocationResponseDto results = locationService.findByAddress(location1);
+
+        assertEquals(results.getAddress(), location1);
+    }
+
+    @Test
+    void 지역_검색_리스트() {
+
+        String location1 = "광역시";
+
+        List<LocationResponseDto> results = locationService.findByAddressAll(location1);
+
+        assertThat(results.size()).isGreaterThan(0);
+
+    }
+
 }
