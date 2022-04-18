fd5abff1998535af70862349d2280ee6793b1417
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-04-18 22:51:44 +0900

    메소드명에 의한 JPA 쿼리 생성 오류로 인해 수정

--- src/main/java/com/project/karrot/src/product/ProductRepository.java
+++ src/main/java/com/project/karrot/src/product/ProductRepository.java
@@ -1,19 +1,14 @@
 package com.project.karrot.src.product;
 
 import com.project.karrot.src.ProductStatus;
-import com.project.karrot.src.category.Category;
-import com.project.karrot.src.location.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    Optional<List<Product>> findByLocation(Long locationId);
+    Optional<List<Product>> findByLocationId(Long locationId);
 
     Optional<List<Product>> findByLocationIdAndCategoryId(Long locationId, Long categoryId); // 지역별 및 카테고리별 상품 조회
 
