87c0da28dae331e15ec85d2344502f5afbf74d2e
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-04-20 01:23:05 +0900

    상품 등록 시 입력값 검증 로직 추가 - BindingResult 적용, 추후 변경 예정

--- src/main/java/com/project/karrot/src/views/MainController.java
+++ src/main/java/com/project/karrot/src/views/MainController.java
@@ -18,6 +18,8 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -69,7 +71,43 @@ public class MainController {
     @LoginCheck
     public ResponseEntity<?> register(@CurrentMemberId Long memberId,
                                       @RequestPart ProductRequestDto productRequestDto,
+                                      BindingResult bindingResult,
                                       @RequestPart(required = false) List<MultipartFile> fileList) {
+
+        // 상품 등록 시 입력값 예외처리
+        // 상품명(productName), 카테고리명(categoryName), 가격(price), 설명(contents) 반드시 필요
+        // 가격은 숫자형만 가능, 범위는 100원 ~ 200만원까지 입력 가능
+        /*if(StringUtils.hasText(productRequestDto.getProductName())) {
+            //bindingResult.addError(new FieldError("productRequestDto", "productName", productRequestDto.getProductName(), false, new String[]{"required.productRequestDto.productName"}, null, "상품명은 필수입니다."));
+            bindingResult.rejectValue("productName", "required");
+        }
+         */
+        ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "productName", "required");
+
+        /*
+        if(StringUtils.hasText(productRequestDto.getCategoryName())) {
+            //bindingResult.addError(new FieldError("productRequestDto", "categoryName", productRequestDto.getCategoryName(), false, new String[]{"required.productRequestDto.categoryName"}, null, "카테고리명은 필수입니다."));
+            bindingResult.rejectValue("categoryName", "required");
+        }
+         */
+        ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "categoryName", "required");
+
+        /*if(StringUtils.hasText(productRequestDto.getContents())) {
+            //bindingResult.addError(new FieldError("productRequestDto", "contents", productRequestDto.getContents(), false, new String[]{"required.productRequestDto.contents"}, null, "상품 설명은 필수입니다."));
+            bindingResult.rejectValue("contents", "required");
+        }
+         */
+        ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "contents", "required");
+
+
+        if(productRequestDto.getPrice() == null || productRequestDto.getPrice() < 100 || productRequestDto.getPrice() > 2000000) {
+            //bindingResult.addError(new FieldError("productRequestDto", "price", productRequestDto.getPrice(), false, new String[]{"range.productRequestDto.price"}, new Object[]{100, 2000000},"가격은 100원~ 200만원까지 입력 가능합니다."));
+            bindingResult.rejectValue("price", "range", new Object[]{100, 2000000}, null);
+        }
+
+        if(bindingResult.hasErrors()) {
+            return new ResponseEntity<>(bindingResult, HttpStatus.BAD_REQUEST);
+        }
 
         List<String> fileUrlList = new ArrayList<>();
 
