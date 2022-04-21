b1a3c021c9a253ab5942ee638a93d88f795aa8dc
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-04-21 16:13:35 +0900

    삭제 메소드 추가 - deleteByProductId

--- src/main/java/com/project/karrot/src/deal/DealRepository.java
+++ src/main/java/com/project/karrot/src/deal/DealRepository.java
@@ -1,7 +1,5 @@
 package com.project.karrot.src.deal;
 
-import com.project.karrot.src.deal.Deal;
-import com.project.karrot.src.deal.dto.DealResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -12,4 +10,7 @@ public interface DealRepository extends JpaRepository<Deal, Long> {
     Optional<List<Deal>> findByMemberId(Long memberId);
 
     Optional<Deal> findByProductId(Long productId);
+
+    Optional<Long> deleteByProductId(Long productId);
+
 }
