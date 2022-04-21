e60a7b8ab6c7d6d88e201ccd06bbbd456a6cf1ef
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-04-21 16:57:21 +0900

    클래스명 규칙 통일 위해 변경 : ProductAndCategoryRes -> ProductAndCategoryResponseDto

--- src/main/java/com/project/karrot/src/product/dto/ProductAndCategoryResponseDto.java
+++ src/main/java/com/project/karrot/src/product/dto/ProductAndCategoryResponseDto.java
@@ -8,18 +8,18 @@ import java.util.List;
 
 @Getter
 @NoArgsConstructor
-public class ProductAndCategoryRes {
+public class ProductAndCategoryResponseDto {
 
     private List<CategoryResponseDto> categoryList;
     private ProductResponseDto productRes;
     private List<ProductResponseDto> productList;
 
-    public ProductAndCategoryRes(List<CategoryResponseDto> categoryList, ProductResponseDto productRes) {
+    public ProductAndCategoryResponseDto(List<CategoryResponseDto> categoryList, ProductResponseDto productRes) {
         this.categoryList = categoryList;
         this.productRes = productRes;
     }
 
-    public ProductAndCategoryRes(List<CategoryResponseDto> categoryList, List<ProductResponseDto> productList) {
+    public ProductAndCategoryResponseDto(List<CategoryResponseDto> categoryList, List<ProductResponseDto> productList) {
         this.categoryList = categoryList;
         this.productList = productList;
     }
--- src/main/java/com/project/karrot/src/views/MainController.java
+++ src/main/java/com/project/karrot/src/views/MainController.java
@@ -9,7 +9,7 @@ import com.project.karrot.src.image.FileUploadService;
 import com.project.karrot.src.location.LocationService;
 import com.project.karrot.src.member.MemberService;
 import com.project.karrot.src.product.ProductService;
-import com.project.karrot.src.product.dto.ProductAndCategoryRes;
+import com.project.karrot.src.product.dto.ProductAndCategoryResponseDto;
 import com.project.karrot.src.product.dto.ProductAndImageResponseDto;
 import com.project.karrot.src.product.dto.ProductRequestDto;
 import com.project.karrot.src.product.dto.ProductResponseDto;
@@ -19,7 +19,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -47,7 +47,7 @@ public class MainController {
         List<CategoryResponseDto> categoryList = categoryService.findAll();
         List<ProductResponseDto> productList = productService.findByLocationAndCategory(categoryAndLocationRequestDto.getLocationId(), categoryAndLocationRequestDto.getCategoryId());
 
-        return new ResponseEntity<>(new ProductAndCategoryRes(categoryList, productList), HttpStatus.OK);
+        return new ResponseEntity<>(new ProductAndCategoryResponseDto(categoryList, productList), HttpStatus.OK);
     }
 
     @ApiOperation(value = "메인 화면 - 지역 검색", notes = "지역을 검색한다")
@@ -70,40 +70,9 @@ public class MainController {
     @PostMapping("/products/new")
     @LoginCheck
     public ResponseEntity<?> register(@CurrentMemberId Long memberId,
-                                      @RequestPart ProductRequestDto productRequestDto,
+                                      @Validated @RequestPart ProductRequestDto productRequestDto,
                                       BindingResult bindingResult,
                                       @RequestPart(required = false) List<MultipartFile> fileList) {
-
-        // 상품 등록 시 입력값 예외처리
-        // 상품명(productName), 카테고리명(categoryName), 가격(price), 설명(contents) 반드시 필요
-        // 가격은 숫자형만 가능, 범위는 100원 ~ 200만원까지 입력 가능
-        /*if(StringUtils.hasText(productRequestDto.getProductName())) {
-            //bindingResult.addError(new FieldError("productRequestDto", "productName", productRequestDto.getProductName(), false, new String[]{"required.productRequestDto.productName"}, null, "상품명은 필수입니다."));
-            bindingResult.rejectValue("productName", "required");
-        }
-         */
-        ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "productName", "required");
-
-        /*
-        if(StringUtils.hasText(productRequestDto.getCategoryName())) {
-            //bindingResult.addError(new FieldError("productRequestDto", "categoryName", productRequestDto.getCategoryName(), false, new String[]{"required.productRequestDto.categoryName"}, null, "카테고리명은 필수입니다."));
-            bindingResult.rejectValue("categoryName", "required");
-        }
-         */
-        ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "categoryName", "required");
-
-        /*if(StringUtils.hasText(productRequestDto.getContents())) {
-            //bindingResult.addError(new FieldError("productRequestDto", "contents", productRequestDto.getContents(), false, new String[]{"required.productRequestDto.contents"}, null, "상품 설명은 필수입니다."));
-            bindingResult.rejectValue("contents", "required");
-        }
-         */
-        ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "contents", "required");
-
-
-        if(productRequestDto.getPrice() == null || productRequestDto.getPrice() < 100 || productRequestDto.getPrice() > 2000000) {
-            //bindingResult.addError(new FieldError("productRequestDto", "price", productRequestDto.getPrice(), false, new String[]{"range.productRequestDto.price"}, new Object[]{100, 2000000},"가격은 100원~ 200만원까지 입력 가능합니다."));
-            bindingResult.rejectValue("price", "range", new Object[]{100, 2000000}, null);
-        }
 
         if(bindingResult.hasErrors()) {
             return new ResponseEntity<>(bindingResult, HttpStatus.BAD_REQUEST);
