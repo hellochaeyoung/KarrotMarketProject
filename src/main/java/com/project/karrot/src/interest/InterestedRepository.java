4ecf61c0227617ba465cc6cff18e9b97aa666bec
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-04-21 16:14:05 +0900

    삭제 메소드 추가 - deleteByProductId

--- src/main/java/com/project/karrot/src/interest/InterestedRepository.java
+++ src/main/java/com/project/karrot/src/interest/InterestedRepository.java
@@ -1,7 +1,5 @@
 package com.project.karrot.src.interest;
 
-import com.project.karrot.src.ProductStatus;
-import com.project.karrot.src.interest.InterestedProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -12,5 +10,7 @@ public interface InterestedRepository extends JpaRepository<InterestedProduct, L
     Optional<List<InterestedProduct>> findByMemberId(Long memberId);
 
     Optional<List<InterestedProduct>> findByProductId(Long productId);
+
+    Optional<Long> deleteByProductId(Long productId);
 
 }
