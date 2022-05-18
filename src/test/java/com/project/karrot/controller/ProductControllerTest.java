b5370b7790e43401b3b700c05d912e1bf77ab694
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-05-18 23:20:27 +0900

    ProductController 테스트 코드 작성 : 상품 댓글 기능 - 조회, 수정, 삭제

--- src/test/java/com/project/karrot/controller/ProductControllerTest.java
+++ src/test/java/com/project/karrot/controller/ProductControllerTest.java
@@ -3,7 +3,10 @@ package com.project.karrot.controller;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.karrot.controller.auth.WithMockCustomMember;
+import com.project.karrot.src.comment.dto.CommentRequestDto;
+import com.project.karrot.src.comment.dto.CommentResponseDto;
 import com.project.karrot.src.interest.dto.InterestedRequestDto;
+import com.project.karrot.src.member.dto.MemberLoginRequestDto;
 import com.project.karrot.src.product.dto.ProductListResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,6 +24,8 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 import javax.transaction.Transactional;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -42,10 +47,22 @@ public class ProductControllerTest {
     private WebApplicationContext context;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws Exception {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
                 .addFilters(new CharacterEncodingFilter("UTF-8", true))
                 .build();
+
+        //given
+        MemberLoginRequestDto loginRequestDto = MemberLoginRequestDto.builder()
+                .email("cyahn@gmail.com")
+                .password("cyahn").build();
+
+        //when
+        mockMvc.perform(MockMvcRequestBuilders.post("/login/auth")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(loginRequestDto))
+                .accept(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8)))
+                .andExpect(status().isOk());
     }
 
     @Test
@@ -125,6 +142,66 @@ public class ProductControllerTest {
         assertThat(count).isEqualTo(0);
     }
 
+    @Test
+    @WithMockCustomMember
+    void 상품_댓글_조회() throws Exception {
 
+        Long productId = 1L;
+
+        String result = mockMvc.perform(MockMvcRequestBuilders.get("/products/" + productId + "/comments"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        List<CommentResponseDto> list = objectMapper.readValue(result, new TypeReference<>() {});
+
+        assertThat(list.size()).isGreaterThan(0);
+    }
+
+    @Test
+    @WithMockCustomMember
+    void 상품_댓글_수정() throws Exception {
+
+        //given
+        Long productId = 1L;
+        String content = objectMapper.writeValueAsString("제가 살게여!");;
+
+        CommentRequestDto requestDto = CommentRequestDto.builder()
+                .commentId(1L)
+                .contents(content)
+                .build();
+
+        //when
+        String result = mockMvc.perform(MockMvcRequestBuilders.put("/products/" + productId + "/comments")
+                                            .contentType(MediaType.APPLICATION_JSON)
+                                            .content(objectMapper.writeValueAsString(requestDto))
+                                            .accept(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8)))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        String contents = objectMapper.readValue(result, new TypeReference<>() {});
+        log.info(contents);
+
+        //then
+        assertThat(contents).isEqualTo("제가 살게여!");
+    }
+
+    @Test
+    @WithMockCustomMember
+    void 상품_댓글_삭제() throws Exception {
+
+        //given
+        Long productId = 1L;
+
+        CommentRequestDto requestDto = CommentRequestDto.builder()
+                .commentId(1L).build();
+
+        //when
+        mockMvc.perform(MockMvcRequestBuilders.delete("/products/" + productId + "/comments")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
+
+
+    }
 
 }
